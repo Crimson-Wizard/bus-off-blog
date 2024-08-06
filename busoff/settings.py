d39f4241f35d3fded91a7b1ac70e772a8e247a29
Michael Lawlor <mikelawlorarb@outlook.com>
2024-08-06 18:17:47 +0000

    add database url to heroku

--- busoff/settings.py
+++ busoff/settings.py
@@ -24,12 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kmrr3-1^w6&+i*@a+$40rir$yv8(t%g+g+s@()1fbegxai7lss'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+# 'django-insecure-kmrr3-1^w6&+i*@a+$40rir$yv8(t%g+g+s@()1fbegxai7lss'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['localhost','.herokuapp.com']
 
 
 # Application definition
