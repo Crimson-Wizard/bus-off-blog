17285c232f6c01b09e010064fd135144c2908ff8
Michael Lawlor <mikelawlorarb@outlook.com>
2024-08-13 15:51:25 +0000

    enable serving of image files

--- media/models.py
+++ media/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
@@ -9,6 +10,7 @@ class Media(models.Model):
     image = models.ImageField(upload_to='media-img/')
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(default=datetime.now)
+    featured_image = CloudinaryField('image', default='placeholder')
     
     class Meta:
         ordering = ["-created_on"]
