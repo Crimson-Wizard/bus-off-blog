8a961e954b2996029766755b7eccc8304831d9ea
Michael Lawlor <mikelawlorarb@outlook.com>
2024-08-24 09:45:19 +0000

    add change to about url docstring

--- about/urls.py
+++ about/urls.py
@@ -1,7 +1,9 @@
 from . import views
 from django.urls import path
 
+"""
+URL patterns for the about section of the website.
+"""
 urlpatterns = [
     path('', views.about_me, name='about'),
-    """URL patterns for the about section of the website."""
 ]
